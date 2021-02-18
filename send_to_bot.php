82a811f617250a63379d21428a9698c78dfcb486
Anton Lomovatskyi <RoffyMonsta@users.noreply.github.com>
2021-02-18 19:44:37 +0200

    fixed smth

--- send_to_bot.php
+++ send_to_bot.php
@@ -3,11 +3,11 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
     $secret_token = "1614286885:AAEhEJD7vXkExtI5o9Z2GkkT50KcOZRC94Y";
     $telegram_id = "471922503";
-    $pesan_teks = $_POST ['pesan_teks'];
+    $textBot = $_POST ['textBot'];
 
-function sendMessage($telegram_id, $pesan_teks, $secret_token) {
+function sendMessage($telegram_id, $textBot, $secret_token) {
     $url = "https://api.telegram.org/bot" . $secret_token . "/sendMessage?chat_id=" . $telegram_id;
-    $url = $url . "&text=" . urlencode($pesan_teks);
+    $url = $url . "&text=" . urlencode($textBot);
     $ch = curl_init();
     $optArray = array(
             CURLOPT_URL => $url,
@@ -18,7 +18,7 @@ function sendMessage($telegram_id, $pesan_teks, $secret_token) {
     curl_close($ch);
 }
 
-sendMessage($telegram_id, $pesan_teks, $secret_token);
+sendMessage($telegram_id, $textBot, $secret_token);
 echo "<script>window.location.href = 'thank_you.html'; </script>";
 }
 ?>
